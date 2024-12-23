d53a12484d3c122059a8ecbbb737c0591b94b42d
kavin <kavin.ned@gmail.com>
2024-12-23 23:26:13 +0800

    'sync'

--- copy.bat
+++ copy.bat
@@ -3,6 +3,16 @@
 :: Copy VS Code settings
 copy "C:\Users\Kavin\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" "."
 
-
 echo Profile copied successfully!
+
+echo.
+
+git add .
+git commit -m 'sync'
+git push
+
+echo.
+
+echo Pushed to GitHub successfully!
+
 pause
